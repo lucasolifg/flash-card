0dfc84bf16d6a66d1f069b656dd84da0b55ba760
lucasolifg <00001102520184sp@al.educacao.sp.gov.br>
2024-11-13 12:24:36 -0300

    top

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Academia',
+    'O que é um supino?',
+    'O supino é um aparelho de academia com ênfase de trabalho no peitoral'
+)
+
+criaCartao(
+    'Brawl Stars',
+    'Quais são as raridades de personagens disponíveis?',
+    'O jogo conta com seis tipos de raridades de brawlers sendo elas: Iniciante,Raro,Super-Raro,Épico,Mítico e Lendário'
+)
+
+criaCartao(
+    'Academia',
+    'Qual o músculo mais difícil de se desenvolver?',
+    'A panturrilha é conhecida por ser um dos músculos mais teimosos quando se trata de crescimento.'
+)
+
+criaCartao(
+    'Futebol',
+    'Como funciona a Copa do Mundo?',
+    'As 32 seleções são distribuídas em 8 chaves com 4 times cada. Os vencedores das quartas avançam para a semifinal. Os times que perdem a semifinal, disputam o 3o lugar. Os times que vencem a semifinal disputam a grande FINAL.'
+)
